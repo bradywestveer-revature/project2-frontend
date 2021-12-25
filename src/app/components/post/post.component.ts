d7ca2d8f2e38b669b5d6281e8d61ca0d3d151105
bradywestveer-revature <93550118+bradywestveer-revature@users.noreply.github.com>
2021-12-25 13:56:28 -0600

    updated post comment: added comment dropdown, updated styles, updated html

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-post',
-  templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+	selector: 'app-post',
+	templateUrl: './post.component.html',
+	styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	showComments : boolean = false;
+	
+	constructor () {}
+	
+	ngOnInit (): void {}
 }
