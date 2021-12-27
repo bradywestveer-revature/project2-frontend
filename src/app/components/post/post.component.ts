8419b05a250e0423763f7868662f27b0f0fd6a9e
bradywestveer-revature <93550118+bradywestveer-revature@users.noreply.github.com>
2021-12-26 22:27:39 -0600

    updated header styling, updated post styling, added multiple image support to posts, added some support for changing content of post component with component variables, added comment posting elements, updated global styles

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -6,7 +6,29 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
+	body : string = "Just got some new plants for my garden! Photos taken by me. 📷 🌸";
+	
+	imageUrls : string [] = [
+		"https://cdn.britannica.com/42/91642-050-332E5C66/Keukenhof-Gardens-Lisse-Netherlands.jpg",
+		"https://media.istockphoto.com/photos/blue-ridge-parkway-scenic-landscape-appalachian-mountains-ridges-picture-id154232673?b=1&k=20&m=154232673&s=170667a&w=0&h=rHdSC9KKqkG8q-KKWfiqMEalaQkleMZ3zxaCYE8Eck8=",
+		"https://www.gardeningknowhow.com/wp-content/uploads/2007/03/flowers-1.jpg",
+		"https://www.thespruce.com/thmb/TIUYmTRJ3NOFnY9LJ6FzMd_9oBc=/2571x1928/smart/filters:no_upscale()/small-garden-ideas-and-inspiration-4101842-01-5e0462c2365e42de86a4f3ebc2152c1b.jpg"
+	];
+	
+	currentImageIndex : number = 0;
+	
+	commentCount : number = 3;
+	likeCount : number = 12;
+	
 	showComments : boolean = false;
+	
+	previousImage () {
+		this.currentImageIndex = Math.max (0, this.currentImageIndex - 1);
+	}
+	
+	nextImage () {
+		this.currentImageIndex = Math.min (this.imageUrls.length - 1, this.currentImageIndex + 1);
+	}
 	
 	constructor () {}
 	
