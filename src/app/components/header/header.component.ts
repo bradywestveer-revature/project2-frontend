8844b5e46955aa2622080eeb97aa73ed2608f35e
bearishtrader <bearish_trader@yahoo.com>
2022-01-01 14:16:05 -0600

    data service set user = <User>{}

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { User } from "src/app/models/User";
 import { ApiService } from "src/app/services/api/api.service";
 import { DataService } from "src/app/services/data/data.service";
 
@@ -11,7 +12,11 @@ export class HeaderComponent implements OnInit {
 	constructor (public dataService : DataService, private apiService : ApiService) {}
 	
 	logout () {
-		this.apiService.deleteSession ();
+		this.apiService.deleteSession ( (data : any) : void => {
+			let id : number = this.dataService.user.id;
+			this.dataService.user = <User>{}
+			this.dataService.users[id] = <User>{}
+		} );
 	}
 	
 	ngOnInit () : void {}
