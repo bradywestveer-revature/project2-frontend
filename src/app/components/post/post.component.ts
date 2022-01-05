a60f77ec199cde0c0a686f684771d7f54a8ab144
bradywestveer-revature <93550118+bradywestveer-revature@users.noreply.github.com>
2022-01-05 13:16:54 -0600

    added youtube video url support to post body, changed post body to a pre to keep formatting, updated global styles

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from "@angular/core";
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
 import { Post } from "src/app/models/Post";
 import { User } from "src/app/models/User";
 import { ApiService } from "src/app/services/api/api.service";
@@ -16,6 +17,8 @@ export class PostComponent implements OnInit {
 	@Input ()
 		post : Post = <Post> {};
 	
+	youTubeVideoUrl : SafeResourceUrl | null = null;
+	
 	currentImageIndex : number = 0;
 	
 	liked : boolean = false;
@@ -24,20 +27,18 @@ export class PostComponent implements OnInit {
 	
 	postCommentInput : string = "";
 	
-	constructor (public dataService : DataService, private apiService : ApiService) {}
+	constructor (public dataService : DataService, private apiService : ApiService, private sanitizer : DomSanitizer) {}
 	
-	previousImage () : void {
+	previousImage = () : void => {
 		this.currentImageIndex = Math.max (0, this.currentImageIndex - 1);
 	}
 	
-	nextImage () : void {
+	nextImage = () : void => {
 		this.currentImageIndex = Math.min (this.post.imageUrls.length - 1, this.currentImageIndex + 1);
 	}
 	
 	//todo do this inline in html somehow
-	getLikeCount = () : number => {
-		return Object.keys (this.post.likes).length;
-	}
+	getLikeCount = () : number => Object.keys (this.post.likes).length;
 	
 	toggleLike = () : void => {
 		this.liked = !this.liked;
@@ -55,9 +56,13 @@ export class PostComponent implements OnInit {
 			//change this.likes on client-side
 			delete this.post.likes [this.dataService.user.id];
 		}
-	}
+	};
 	
 	postComment = () : void => {
+		if (this.postCommentInput === "") {
+			return;
+		}
+		
 		this.apiService.createComment (this.post.id, this.postCommentInput, () : void => {
 			this.apiService.getPost (this.post.id, (data : any) : void => {
 				this.post = data.data;
@@ -65,19 +70,25 @@ export class PostComponent implements OnInit {
 		});
 		
 		this.postCommentInput = "";
-	}
+	};
 	
 	commentInputKeyDown = (event : KeyboardEvent) : void => {
 		if (event.key === "Enter") {
 			this.postComment ();
 		}
-	}
+	};
 	
 	ngOnInit () {
-		//todo wait until we get posts in Main/Profile
+		//todo wait until we get posts in Main/Profile?
 		
 		if (this.post.likes [this.dataService.user.id] !== undefined) {
 			this.liked = true;
+		}
+		
+		const matches = [...this.post.body.matchAll (/(youtube\.com|youtu\.be)\/(watch\?v=)?(\w{11})/g)];
+		
+		if (matches.length > 0) {
+			this.youTubeVideoUrl = this.sanitizer.bypassSecurityTrustResourceUrl ('https://www.youtube-nocookie.com/embed/' + matches [0] [3]);
 		}
 	}
 }
