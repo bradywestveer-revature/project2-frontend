0438f94798b0ffca25dd458ba2dd07d296c93ea7
bearishtrader <bearish_trader@yahoo.com>
2022-01-06 05:28:57 -0600

    New module files for password reset feature

---
+++ src/app/pages/reset-password/reset-password.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api/api.service';
+
+@Component({
+  selector: 'app-reset-password',
+  templateUrl: './reset-password.component.html',
+  styleUrls: ['./reset-password.component.css']
+})
+export class ResetPasswordComponent implements OnInit {
+
+  errorTextClass : string = "errorText white";
+  errMessage: string = "Forgot Password";
+  emailInput: string = "";
+
+  constructor(private apiServ:ApiService) { }
+
+  ngOnInit(): void {
+    this.errMessage = "Forgot Password?";
+  }
+
+  isValidEmail = (email : string) : boolean => {
+		let success : boolean;
+
+		let regexp = new RegExp ("[\\w-\]+@[\\w-]+\\.[a-zA-z\]+");
+		success = regexp.test (email);
+		
+		return success;
+	};
+
+  keyDown = (event : KeyboardEvent) => {
+		if (event.key === "Enter") {
+			this.resetPassword ();
+		}
+	};
+
+  resetPassword = () => {
+    if (!this.isValidEmail(this.emailInput)) {
+      this.errorTextClass = "errorText";
+      this.errMessage = "Invalid e-mail address.";
+      return;
+    }
+    this.errorTextClass = "errorText";
+    this.errMessage = "Sending password reset e-mail link, please be patient..."
+    this.apiServ.resetPassword(this.emailInput, (data:any) : void => {
+      this.errorTextClass = "errorText";
+      this.errMessage = data.message;
+    })
+  }
+
+}
